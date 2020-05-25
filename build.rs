7cac40bbcfdca5b4b9577f9f229ef5d74e55411b
Christopher Chalmers <c.chalmers@me.com>
2020-05-26 00:34:46 +0100

    Get bindgen working with a patch

--- build.rs
+++ build.rs
@@ -7,47 +7,65 @@ fn main() {
         .flag_if_supported("-std=c++14")
         .compile("cxxbridge-demo");
 
-    // let bindings = bindgen::Builder::default()
-    //     .header("cbits/lg.hpp")
-    //     .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-    //     .whitelist_recursively(false)
-    //     .whitelist_function("mlperf::c.*")
-    //     .whitelist_type("mlperf::c::.*")
-    //     .whitelist_type("mlperf::TestSettings")
-    //     .whitelist_type("mlperf::LogSettings")
-    //     .whitelist_type("mlperf::LoggingMode")
-    //     .whitelist_type("mlperf::LogOutputSettings")
-    //     .new_type_alias("std::string")
-    //     .whitelist_type("std::string")
-    //     .opaque_type("std::string")
-    //     .no_copy("mlperf::LogOutputSettings")
-    //     .no_copy("mlperf::LogSettings")
-    //     // .default_alias_style(bindgen::AliasVariation::NewType)
-    //     // .opaque_type("mlperf::LogOutputSettings")
-    //     .whitelist_type("mlperf::QuerySample.*")
-    //     .whitelist_type("mlperf::Test.*")
-    //     .whitelist_type("mlperf::ResponseId")
-    //     .size_t_is_usize(true)
-    //     .ignore_methods()
-    //     .impl_debug(true)
-    //     .enable_cxx_namespaces()
-    //     .clang_arg("-x")
-    //     .clang_arg("c++")
-    //     .layout_tests(false)
-    //     // .rustified_non_exhaustive_enum("Test.*")
-    //     .default_enum_style(bindgen::EnumVariation::Rust { non_exhaustive: true })
-    //     .generate()
-    //     .expect("Unable to generate bindings");
+    let bindings = bindgen::Builder::default()
+        .header("cbits/lg.hpp")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .whitelist_recursively(false)
+        .whitelist_function("mlperf::c.*")
+        .whitelist_type("mlperf::c::.*")
+        .whitelist_type("mlperf::TestSettings")
+        .whitelist_type("mlperf::LogSettings")
+        .whitelist_type("mlperf::LoggingMode")
+        .whitelist_type("mlperf::LogOutputSettings")
+        .whitelist_type("std::string")
+        .opaque_type("std::string")
+        // this doesn't seem to work so we patch it later instead
+        // .new_type_alias("std::string")
 
-    // // // Write the bindings to the $OUT_DIR/bindings.rs file.
-    // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    // bindings
-    //     .write_to_file(out_path.join("bindings.rs"))
-    //     .expect("Couldn't write bindings!");
+        // don't copy because it constains a c++ std::string
+        .no_copy("mlperf::LogOutputSettings")
+        .no_copy("mlperf::LogSettings")
+        .whitelist_type("mlperf::QuerySample.*")
+        .whitelist_type("mlperf::Test.*")
+        .whitelist_type("mlperf::ResponseId")
+        .size_t_is_usize(true)
+        // there's a method on TestSettings which we can't use because it uses C++ strings
+        .ignore_methods()
+        .impl_debug(true)
+        .enable_cxx_namespaces()
+        // these two ares are the only way I could get it to work with c++
+        .clang_arg("-x")
+        .clang_arg("c++")
+        .layout_tests(false)
+        .default_enum_style(bindgen::EnumVariation::Rust { non_exhaustive: true })
+        .generate()
+        .expect("Unable to generate bindings");
 
-    // bindings
-    //     .write_to_file("src/bindings.rs")
-    //     .expect("Couldn't write bindings!");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let bindings_path = out_path.join("bindings.rs");
+    bindings
+        .write_to_file(&bindings_path)
+        .expect("Couldn't write bindings!");
+
+    // bindgen's `opaque_type` doesn't seems to woth with `new_type_alias` so manually patch it.
+    // Also add some documentation and make the interior non-public (which bindgen's type alias
+    // doesn't do).
+    let string_match = "pub type string = \\(.*\\);";
+    let string_replacement = "#[repr(transparent)]\\n/// A C++ string\\npub struct string(\\1);";
+    let sed_expr = format!("s:{}:{}:", string_match, string_replacement);
+
+    let output = std::process::Command::new("sed")
+        .args(&["-ie", &sed_expr, bindings_path.to_str().expect("bad bindings path")])
+        .output()
+        .expect("failed to patch bindings");
+
+    if !output.status.success() {
+        use std::io::{self, Write};
+        eprintln!("patching failed:");
+        io::stderr().write_all(&output.stderr).unwrap();
+    }
 
     println!("cargo:rustc-link-lib=mlperf_loadgen");
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ mod ffi {
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
 
-    // include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-    include!("bindings.rs");
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
 #[cxx::bridge(namespace = mlperf)]
